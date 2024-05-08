bd83e18f0c7a15bf1e09a5da6a7c60645ff5aad9
Stefano <s.macinaleone@gmail.com>
2024-05-08 11:42:20 +0200

    add : project

---
+++ GymAiBackend/src/main/java/com/stenfra/GymAi/GymAiApplication.java
@@ -0,0 +1,13 @@
+package com.stenfra.GymAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAiApplication.class, args);
+	}
+
+}
---
+++ GymAiBackend/src/test/java/com/stenfra/GymAi/GymAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stenfra.GymAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
