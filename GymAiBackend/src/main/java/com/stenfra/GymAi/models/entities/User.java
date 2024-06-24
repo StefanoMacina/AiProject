688052ea4c74e808a2bc0278266c46b37cc8afa8
macina <macina@key-4.com>
2024-06-24 10:46:38 +0200

    add userdetails

---
+++ GymAiBackend/src/main/java/com/stenfra/GymAi/models/entities/User.java
@@ -0,0 +1,91 @@
+package com.stenfra.GymAi.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String firstname, lastname, email, username;
+
+    private LocalDate birthdate;
+
+    private Integer age;
+
+    private String passwd;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return passwd;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User(String firstname, String lastname, String email, LocalDate birthdate, Integer age, String passwd, Role role) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.age = Period.between(birthdate, LocalDate.now()).getYears();
+        this.passwd = passwd;
+        this.role = role;
+    }
+}
+
+enum Role{
+    ROLE_ADMIN,
+    ROLE_USER
+}
