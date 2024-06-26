864b7bd970d5f65e1fc94e26a5220d16150145b7
macina <macina@key-4.com>
2024-06-26 18:03:45 +0200

    add authguard

--- frontend/src/app/auth/guardians/auth.guard.ts
+++ frontend/src/app/auth/guardians/auth.guard.ts
@@ -1,5 +1,11 @@
 import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../authService/auth.service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  return () => {
+    const authService: AuthService=inject(AuthService);
+
+    
+  };
 };
