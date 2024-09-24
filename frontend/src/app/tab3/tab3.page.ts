2fc6f8be31f4c281b47e3118b25dcb37067ee3cd
Stefano <s.macinaleone@gmail.com>
2024-09-24 23:29:09 +0200

    last commit

--- frontend/src/app/tab3/tab3.page.ts
+++ frontend/src/app/tab3/tab3.page.ts
@@ -25,7 +25,6 @@ export class Tab3Page {
     this.$service.getUserInfo().subscribe(
       data => {
         this.user = data
-        //console.log(data)
       },
       error => console.error("failed to fetch user info")
     ) 
